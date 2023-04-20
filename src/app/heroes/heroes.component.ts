0466e1d6e1e5452d4dff3a9466f6bf105dc46b7e
Rushal Maharjan <numb123jhiyo@gmail.com>
2023-04-20 21:38:37 +0545

    added style CSS

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../hero_list';
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
